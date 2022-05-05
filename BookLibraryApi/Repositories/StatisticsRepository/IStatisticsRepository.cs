265341150712e219e953938c5a432583b84a53ae
muhammadmb <muhammadmagdyje@gmail.com>
2022-05-06 01:56:12 +0200

    Add Statistics Repository

---
+++ BookLibraryApi/Repositories/StatisticsRepository/IStatisticsRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookLibraryApi.Models.StatisticsModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookLibraryApi.Repositories.StatisticsRepository
+{
+    public interface IStatisticsRepository
+    {
+        Task<Statistics> GetStatistics();
+        Task<List<BookStatistic>> GetBooksStatistics();
+    }
+}
---
+++ BookLibraryApi/Repositories/StatisticsRepository/StatisticsRepository.cs
@@ -0,0 +1,72 @@
+﻿using BookLibraryApi.Contexts;
+using BookLibraryApi.Models.StatisticsModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibraryApi.Repositories.StatisticsRepository
+{
+    public class StatisticsRepository : IStatisticsRepository, IDisposable
+    {
+        private readonly BookContext _context;
+        public StatisticsRepository(BookContext context)
+        {
+            _context = context ??
+                throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<Statistics> GetStatistics()
+        {
+            var books = await _context.Books.ToListAsync();
+            var authors = await _context.Authors.ToListAsync();
+            var reviews = await _context.Reviews.ToListAsync();
+            var genres = await _context.Genres.ToListAsync();
+            var feedback = await _context.Feedbacks.ToListAsync();
+            var suggestions = await _context.Suggestions.ToListAsync();
+
+            return new Statistics
+            {
+                Books = books.Count,
+                Authors = authors.Count,
+                Reviews = reviews.Count,
+                Genres = genres.Count,
+                Feedbacks = feedback.Count,
+                Suggestions = suggestions.Count
+            };
+        }
+
+        public async Task<List<BookStatistic>> GetBooksStatistics()
+        {
+            var books = await _context.Books
+                .Where(b => b.AddedDate.Year == DateTime.Now.Year)
+                .ToListAsync();
+
+            var rt = books.GroupBy(b => b.AddedDate.Month)
+                .Select(b => new BookStatistic
+                {
+                    Month = b.Key,
+                    Books = b.Count()
+                })
+                .OrderBy(b => b.Month)
+                .ToList();
+
+            return new List<BookStatistic>(rt);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // dispose resources when needed
+            }
+        }
+    }
+}
