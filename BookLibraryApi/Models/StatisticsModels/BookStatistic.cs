e4cb188f7eecaf42b43ef2b8a0bbb3ada7434f84
muhammadmb <muhammadmagdyje@gmail.com>
2022-05-05 20:48:39 +0200

    Add Statistics and book statistics

---
+++ BookLibraryApi/Models/StatisticsModels/BookStatistic.cs
@@ -0,0 +1,9 @@
+﻿namespace BookLibraryApi.Models.StatisticsModels
+{
+    public class BookStatistic
+    {
+        public int Books { get; set; }
+
+        public int Month { get; set; }
+    }
+}
---
+++ BookLibraryApi/Models/StatisticsModels/Statistics.cs
@@ -0,0 +1,17 @@
+﻿namespace BookLibraryApi.Models.StatisticsModels
+{
+    public class Statistics
+    {
+        public int Books { get; set; }
+
+        public int Authors { get; set; }
+
+        public int Genres { get; set; }
+
+        public int Reviews { get; set; }
+
+        public int Feedbacks { get; set; }
+
+        public int Suggestions { get; set; }
+    }
+}
