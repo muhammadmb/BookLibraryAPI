cbbd9d2149317db8f2144be367e8285c507c310a
muhammadmb <muhammadmagdyje@gmail.com>
2022-05-06 21:31:57 +0200

    Add Statistics Controller

---
+++ BookLibraryApi/Controllers/StatisticsController.cs
@@ -0,0 +1,40 @@
+﻿using BookLibraryApi.Repositories.StatisticsRepository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace BookLibraryApi.Controllers
+{
+    [ApiController]
+    [Route("api/statistics")]
+    [EnableCors("demoPolicy")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IStatisticsRepository _statisticsRepository;
+        public StatisticsController(IStatisticsRepository statisticsRepository)
+        {
+            _statisticsRepository = statisticsRepository ??
+                throw new ArgumentNullException(nameof(statisticsRepository));
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetStatistics()
+        {
+            var statistics = await _statisticsRepository.GetStatistics();
+
+            return Ok(statistics);
+        }
+
+        [HttpGet("Books")]
+        public async Task<IActionResult> getBooksStatistics()
+        {
+            var sts = await _statisticsRepository.GetBooksStatistics();
+
+            return Ok(sts);
+        }
+    }
+}
